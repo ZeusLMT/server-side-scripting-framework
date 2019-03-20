c7d3d7709188b4ea45079c8184a25b9e2c765529
ZeusLMT <lemanhtuan275@gmail.com>
2019-03-21 01:14:53 +0200

    install mongoose and create schema + model

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 require('dotenv').config();
+
+const app = express();
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const photoSchema = new Schema({
+  id: Number,
+  time: Date,
+  category: String,
+  title: String,
+  details: String,
+  coordinates: { lat: String, lng: String },
+  thumbnail: String,
+  image: String,
+  original: String
+});
+
+const photoModel = mongoose.model('photos', photoSchema);
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_MODEL}`).then(() => {
+  console.log('Connected successfully.');
+  app.listen(process.env.APP_PORT);
+}, err => {
+  console.log('Connection to db failed: ' + err);
+});
