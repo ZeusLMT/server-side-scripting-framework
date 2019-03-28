db8fb1817ae4125e8040b71ed82eb7ad17dc1358
ZeusLMT <lemanhtuan275@gmail.com>
2019-03-28 11:46:55 +0200

    uncomment https redirecting for jelastic deploy

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ app.set('view engine', 'ejs');
 const mongoose = require('mongoose');
 
 //https redirecting
-// app.enable('trust proxy');
-// app.use ((req, res, next) => {
-//   if (req.secure) {
-//     // request was via https, so do no special handling
-//     next();
-//   } else {
-//     // request was via http, so redirect to https
-//     res.redirect('https://' + req.headers.host + req.url);
-//   }
-// });
+app.enable('trust proxy');
+app.use ((req, res, next) => {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 
 
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_MODEL}`)
