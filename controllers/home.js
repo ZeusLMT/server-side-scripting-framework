bdb0988bb443229c31ca0a05828724195899bcf0
ZeusLMT <lemanhtuan275@gmail.com>
2019-04-01 11:03:33 +0300

    fix bug with redirecting home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,14 @@
 exports.renderHome = (req, res) => {
-  if (req.params.category !== undefined) {
-    res.render('home', {category: req.params.category});
+  if (req.query.category !== undefined) {
+    res.render('home', {category: req.query.category});
   } else {
     res.render('home', {category: 'all'});
   }
 };
 
 exports.redirectToCategory = (req, res) => {
+  if (req.body.category === 'all') {
+    res.redirect('/');
+  }
   res.redirect(`/?category=${req.body.category}`);
 };
