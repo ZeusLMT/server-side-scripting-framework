acc224c39ddf245c4123f2b4f6cdb7e832da26ba
ZeusLMT <lemanhtuan275@gmail.com>
2019-04-05 10:05:51 +0300

    Add login test node to test session

--- routers/login.js
+++ routers/login.js
@@ -10,4 +10,10 @@ router.post('/', controller.handleLogin);
 
 router.get('/failed', controller.onLoginFail);
 
+router.get('/test', (req, res) => {
+  if(req.user !== undefined)
+    return res.send(`Hello ${req.user.username}!`);
+  res.send('Hello Secure World!');
+});
+
 module.exports = router;
